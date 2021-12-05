b103a8139125ccedda6c5403fbc89252aae2c426
fataoup <wz128595@mail.ustc.edu.cn>
2021-12-05 14:40:26 +0000

    rnn model

--- model/Model.py
+++ model/Model.py
@@ -1,35 +1,49 @@
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.optim import optimizer
+import numpy as np
 from torch.utils.data import Dataset, DataLoader
-from data_preprocess import load_and_process_data
+from torch.utils.data.dataset import T
+from data_preprocess import get_max_seqlen, load_and_process_data, load_text
 
 n_epochs = 100 
-n_entities = 14540 
+n_entities = 14541 
 n_relations = 237
-device = torch.device('cuda:0')
+batch_size = 16
+n_hidden = 100
+max_seqlen = 200
+
+device = torch.device('cuda:7' if torch.cuda.is_available() else 'cpu')
 
 class Model(nn.Module):
-
-    def __init__(self, n_tokens, embedding_dim, hidden_dim) -> None:
+    # to be modified
+    def __init__(self) -> None:
         super(Model, self).__init__()
-        self.n_tokens = n_tokens
-        self.embedding_dim = embedding_dim
-        self.embedding: nn.Embedding = nn.Embedding(num_embeddings=n_tokens, embedding_dim=embedding_dim)
-        # nn.Embedding: 输入index， 输出该index的embedding. shape = n_tokens
-        self.linear1 = nn.Linear(2 * embedding_dim, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, n_tokens)
+        self.n_entities = n_entities
+        self.n_relations = n_relations
+        # use RNN to calculate semantic similarity
+        self.encoder = nn.RNN(input_size=n_class, hidden_size=n_hidden)
+        self.decoder = nn.RNN(input_size=n_class, hidden_size=n_hidden)
+        self.fc = nn.Linear(n_hidden, n_class)
+
+    def forward(self, enc_input_h, enc_input_r, enc_hidden, dec_input):
+        # to be modified
+        enc_input_h = enc_input_h.transpose(0, 1)
+        enc_input_r = enc_input_r.transpose(0, 1)
 
 
-    def forward(self, input_indices):
-        input_vecs = self.embedding(input_indices) # len(input_indices) = 2 , shape = (batchsize, 2, embedding_dim)
-        input_flatten = input_vecs.view(-1, 1, 2 * self.embedding_dim) # shape: (batchsize, 1, 2 * embedding_dim), flatten
-        out = self.linear1(input_flatten)
-        out = F.relu(out) 
-        out = self.linear2(out)
-        probs = F.softmax(out, dim=-1) # probs: 1 * n_tokens
-        return probs
+        dec_input = dec_input.transpose(0, 1)
+        #enc_hidden : [1, batch_size, hidden_size]
+        _, enc_states = self.encoder(enc_input_h, enc_hidden)
+        
+        _, enc_states = self.encoder(enc_input_r, enc_states)
+
+        # outputs: [maxlen + 1, batch_size, hidden_size]
+        outputs, _ = self.decoder(dec_input, enc_states)
+        outputs = self.fc(outputs)
+        
+        return outputs
 
 
 class TripletDataset(Dataset):
@@ -42,32 +56,51 @@ class TripletDataset(Dataset):
         return self.x.shape[0]
 
     def __getitem__(self, index):
-        return self.x[index], self.y[index]
+        h = self.x[index][0]
+        r = self.x[index][1]
+        t = self.y[index][0]
+
+        words_h = [word_dict[n] for n in entities_dict[h].split()]
+        words_h += [word_dict['Pad']] * (max_seqlen - len(words_h))
+
+        words_r = [word_dict[n] for n in relations_dict[r].split()]
+        words_r += [word_dict['Pad']] * (max_seqlen - len(words_r))
+
+        words_t = [word_dict[n] for n in entities_dict[t].split()]
+        words_t += [word_dict['Pad']] * (max_seqlen - len(words_t))
+
+        head_seq = np.eye(n_class)[words_h]
+        rela_seq = np.eye(n_class)[words_r]
+        #c = np.eye(n_class)[words_t]
+        tail_seq = np.eye(n_class)[[word_dict['Start']] + words_t]
+
+
+        return torch.FloatTensor(head_seq), torch.FloatTensor(rela_seq), torch.FloatTensor(tail_seq), \
+            torch.FloatTensor(tail_seq + [word_dict['End']])
+
 
 
 def train(model, train_dataloader, device, optimizer, criterion, n_epochs):
     model.train()
-    for epoch in range(n_epochs):
-        for i, (feature, label) in enumerate(train_dataloader):
-            label = label.to(device)
-            feature[:,1] += n_entities            
-            feature = feature.to(device)
+    for epoch in tqdm(range(n_epochs)):
 
-            optimizer.zero_grad()
-            output = model(feature)
-            label = label.to(torch.int64)
-            label = F.one_hot(label, model.n_tokens).to(torch.float64)
-            output = torch.flatten(output, start_dim=-2)
-            label = torch.flatten(label, start_dim=-2)
-
-            loss = criterion(output, label)
+        for i, (h, r, output_t, target_t) in enumerate(train_dataloader):
+            optimizer.zero_grad()            
+            hidden_state = torch.zeros(1, batch_size, n_hidden).to(device)
+            h = h.to(device)
+            r = r.to(device)
+            output_t = output_t.to(device)
+            target_t = target_t.to(device)
+            output = model(h, r, hidden_state, output_t)
+            output = output.transpose(0, 1)
+            loss = criterion(output, target_t)
 
             loss.backward()
             optimizer.step()
 
             if i % 100 == 0:
                     print('Train Epoch: {}/{} [{}/{}]\tLoss: {:.6f}'.format(
-                        epoch, n_epochs, i * len(feature), len(train_dataloader.dataset), loss.item()))
+                        epoch, n_epochs, i * len(h), len(train_dataloader.dataset), loss.item()))
 
     torch.save(model, "../output/Model.pkl")
 
@@ -86,14 +119,33 @@ def test(model, test_dataloader, device, criterion):
 
 
 if __name__ == '__main__':
-    train_feature, train_label, test_feature, test_label = load_and_process_data('../dataset/train.txt', '../dataset/dev.txt')
+    train_feature, train_label, test_feature, test_label = \
+        load_and_process_data('/data/wentaozhang/zwt/webinfo_lab2/dataset/train.txt',\
+             '/data/wentaozhang/zwt/webinfo_lab2/dataset/dev.txt')
+    
+    entities_dict, relations_dict, word_dict, index_dict \
+        = load_text('/data/wentaozhang/zwt/webinfo_lab2/dataset/entity_with_text.txt', \
+            '/data/wentaozhang/zwt/webinfo_lab2/dataset/relation_with_text.txt')    
+    # entities_dict : key: entity id . value: entity text
+    # relations_dict : key: relation id. value: relation text
+
+
+    word_dict['Pad'] = len(word_dict)
+    word_dict['Start'] = len(word_dict)
+    word_dict['End'] = len(word_dict)
+
+    max_key, max_seqlen = get_max_seqlen(entities_dict, relations_dict)
+    
     train_Dataset = TripletDataset(train_feature, train_label)
     test_Dataset = TripletDataset(test_feature, test_label)
-    train_DataLoader = DataLoader(train_Dataset, batch_size=64)
-    test_Dataset = DataLoader(test_Dataset, batch_size=64)
-    model = Model(n_tokens=n_entities + n_relations, embedding_dim = 64, hidden_dim=32).to(device)
+    train_DataLoader = DataLoader(train_Dataset, batch_size=batch_size)
+    test_Dataset = DataLoader(test_Dataset, batch_size=batch_size)
 
+    n_class = len(word_dict)  # vocab list
+
+
+    model = Model().to(device)
+    #model = Model()
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr = 0.01)
-
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.01)
     train(model=model, train_dataloader=train_DataLoader, device=device, optimizer=optimizer, criterion=criterion, n_epochs=100)
--- model/data_preprocess.py
+++ model/data_preprocess.py
@@ -2,11 +2,11 @@ import numpy as np
 
 def load_and_process_data(train_filename, test_filename):
 
-    train_xy = np.loadtxt(train_filename, dtype=int)
+    train_xy = np.loadtxt(train_filename, dtype=str)
     train_feature = train_xy[:, 0:-1]
     train_label = train_xy[:, [-1]]
 
-    test_xy = np.loadtxt(test_filename, dtype=int)
+    test_xy = np.loadtxt(test_filename, dtype=str)
     test_feature = test_xy[:, 0:-1]
     test_label = test_xy[:, [-1]]
 
@@ -17,7 +17,52 @@ def load_and_process_data(train_filename, test_filename):
 
     return train_feature,train_label,test_feature,test_label 
 
+def load_text(entities_filename, relations_filename):
+    entities_dict = {}
+    relations_dict = {}
+    sentences_e = []
+    sentences_r = []
+
+    with open(entities_filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.rstrip().split('\t')
+            entities_dict[line[0]] = line[1]
+            sentences_e.append(line[1])
+
+    with open(relations_filename) as f:
+        lines = f.readlines()
+        for line in lines:
+            line = line.rstrip().split('\t')
+            relations_dict[line[0]] = line[1]
+            sentences_r.append(line[1])
+
+    word_list = ' '.join(sentences_e + sentences_r).split()
+    word_list = list(set(word_list)) 
+    word_dict = {w: i for i, w in enumerate(word_list)}
+    index_dict = {i: w for i, w in enumerate(word_list)}
+
+
+    return entities_dict, relations_dict, word_dict, index_dict
+
+def get_max_seqlen(entities_dict: dict, relations_dict: dict):
+    max_seqlen = 0
+    for key, value in entities_dict.items():
+        list = value.split()
+        if len(list) > max_seqlen:
+            max_seqlen = len(list)
+            max_key = key
+    
+    for key, value in relations_dict.items():
+        list = value.split()
+        if len(list) > max_seqlen:
+            max_seqlen = len(list)
+            max_key = key
+
+    return max_key, max_seqlen
+
+
 
 if __name__ == "__main__":
     train_feature, train_label, test_feature, test_label = load_and_process_data('../dataset/train.txt', '../dataset/dev.txt')
-
+    entities_dict, relations_dict = load_text('../dataset/entity_with_text.txt', '../dataset/relation_with_text.txt')
